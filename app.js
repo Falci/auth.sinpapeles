42b22eefb1b4acab419a4b036c1fd72677b53730
Fernando Falci <fernando.falci@adevinta.com>
2020-08-22 23:23:23 +0200

    Fix hostname

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, _, next) => {
-  app.locals.canInstall = req.host.indexOf("localhost") > -1;
+  app.locals.canInstall = req.hostname.indexOf("localhost") > -1;
 
   next();
 });
