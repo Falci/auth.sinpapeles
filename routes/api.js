03dd530b9e61d589b9b577a5e68ba5ad6bbf70f2
Fernando Falci <fernando.falci@adevinta.com>
2020-08-22 22:51:23 +0200

    Using custom DNS port

--- routes/api.js
+++ routes/api.js
@@ -3,27 +3,29 @@ const express = require("express");
 const config = require("config");
 const router = express.Router();
 
-const dig = config.get("dig");
+const { host, port } = config.get("dig");
 
 const getTXT = (domain) =>
   new Promise((resolve, reject) => {
-    console.log(`dig ${dig && `@${dig}`} ${domain} TXT +short`);
-    exec(`dig ${dig && `@${dig}`} ${domain} TXT +short`, (error, out) => {
-      if (error) {
-        return reject(error);
+    exec(
+      `dig ${host && `@${host}`} ${port && `-p ${port}`} ${domain} TXT +short`,
+      (error, out) => {
+        if (error) {
+          return reject(error);
+        }
+
+        const txt = out
+          .split("\n")
+          .map((line) => line.substring(1, line.length - 1)) // remove "quotes"
+          .find((line) => line.indexOf("auth=") === 0);
+
+        if (!txt) {
+          return reject(new Error("auth TXT not found"));
+        }
+
+        resolve(txt.substring(5));
       }
-
-      const txt = out
-        .split("\n")
-        .map((line) => line.substring(1, line.length - 1)) // remove "quotes"
-        .find((line) => line.indexOf("auth=") === 0);
-
-      if (!txt) {
-        return reject(new Error("auth TXT not found"));
-      }
-
-      resolve(txt.substring(5));
-    });
+    );
   });
 
 router.post("/", async (req, res) => {
